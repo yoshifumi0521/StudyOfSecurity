9ec08568248f1df569d9e172cb84628af5c300bb
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-02 22:51:02 +0900

    XSSのディレクトリを追加

---
+++ xss/index1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>クロスサイトスクリプティングについて</title>
+</head>
+<body>
+	
+	
+	
+	
+	<script>
+	
+		
+		
+		
+		
+
+
+	</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
