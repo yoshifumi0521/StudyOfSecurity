e330a81b4f4174365b36fd9249e524a2028cd6dc
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-03 06:57:35 +0900

    ENT_QUOTESが無い場合の攻撃パターン

--- xss/index1.php
+++ xss/index1.php
@@ -6,7 +6,19 @@
 </head>
 <body>
 	
-	
+   <h1>htmlspecialcharsの第二引数にENT_QUOTESが無い場合。</h1>
+   <p>シングルコーテーション''がエスケープされないので、攻撃の対象
+   になる。</p>
+
+
+   <p>パターン①　---'onmouseover=alert(document.cookie)'</p>
+   <p>パターン②　---'onfocus=alert(documet.cookie) autofocus'</p>
+
+   <form method="GET" action="?">キーワード
+      <input type="text" name="key" value='<?php echo (htmlspecialchars($_GET["key"]))?>'>
+      <input type="submit" value="検索">
+   </form>
+
 	
 	
 	<script>
