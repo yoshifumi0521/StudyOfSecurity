b27e7c9769ba6fbfda1988fef24433a15fab633f
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-03 11:09:22 +0900

    form.phpを実装

--- XMLHttpRequest/form.php
+++ XMLHttpRequest/form.php
@@ -6,7 +6,13 @@
 </head>
 <body>
 
+   <h1>IPアドレスをいれると、国名や都市の位置がでてくる。</h1>
 
+   <form method="GET" action="http://api.hostip.info/get_html.php">
+      <input type="text" name="ip" value="12.34.56.78">
+      <input type="hidden" name="position" value="true">
+      <input type="submit" value="送信">
+   </form>
 
 
 
