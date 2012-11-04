e085a14497081f852338c63ea51514370117a477
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-05 02:55:47 +0900

    iframeを使ったキーロガーの攻撃の対処を書いた。

---
+++ iframe/frame.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>iframe内容 </title>
+</head>
+<body style="background:red">
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
---
+++ iframe/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title> iframeを挿入する。 </title>
+    <!--　次のようなスタイルを設定すれば、iframeをつかったキーロガーを可視化できる。 -->
+    <style>
+      iframe,frame,object,applet{
+         opacity: 0.50 !important;
+      }
+
+    </style>
+</head>
+<body style="background:blue">
+
+<iframe src="frame.php" style="width:200px,height:200px">iframe未対応の場合</iframe>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
