43c39cda665528741ab024a1d157f4653737e5c8
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-04 22:35:32 +0900

    XMLHttpRequest.phpを追加

--- XMLHttpRequest/XMLHttpRequest.php
+++ XMLHttpRequest/XMLHttpRequest.php
@@ -2,10 +2,40 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>XMLHttpRequestでクロスドメイン処理を行う。</title>
+    <title>XMLHttpRequest level2でクロスドメイン処理を行う。</title>
 </head>
 <body>
-	
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   <script>
+      
+      var xhr = new  XMLHttpRequest();
+      var url = "http://api.hostip.info/get_html.php?ip=";
+      //XMLHttpRequestの設定をする。
+      xhr.open("GET",url+"12.34.56.78",false);
+      //非同期通信を開始する。
+      xhr.send(null);
+      //レスポンスがしっかりかえってきたらする処理。
+      if (xhr.status == 200 )
+      {
+         console.log(xhr.responseText);
+      }
+
+
+   </script>
+
+
+
+
 
 
 
